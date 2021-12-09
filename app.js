7140805b4286116267f20f4d0f160bd8f22e39eb
Akshat <Akshatdx12345@gmail.com>
2021-12-09 22:27:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const arrows = document.querySelectorAll('.arrow');
+const ans = document.querySelectorAll('.a')
+
+for(let a of ans){
+    a.addEventListener('click',function(){
+        console.log('asdasd');
+    })
+}
